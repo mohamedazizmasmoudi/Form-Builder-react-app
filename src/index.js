841c73383abf5ba1589a72fc8801ab589feb24aa
mohamedazizmasmoudi <55623593+mohamedazizmasmoudi@users.noreply.github.com>
2021-02-28 09:38:59 +0100

    progress vote app

--- src/index.js
+++ src/index.js
@@ -3,3 +3,9 @@ import ReactDOM from "react-dom";
 import App from "./App";
 
 ReactDOM.render(<App />, document.getElementById("root"));
+window.addEventListener('load', () => {
+    navigator.serviceWorker
+      .register('./serviceworker.js')
+      .then((reg) => console.log('Success: ', reg.scope))
+      .catch((err) => console.log('Failure: ', err));
+  });
